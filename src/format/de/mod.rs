d37571089ad094025484b0a6b8e990a1948f1d7c
Tobias Peters <tobias.peters@kreativeffekt.at>
2025-12-14 19:11:45 +0100

    enable test for deserializing sequences with serde

--- src/format/de/mod.rs
+++ src/format/de/mod.rs
@@ -49,64 +49,25 @@ mod tests {
         assert_eq!(expected, from_pdr(&j).unwrap());
     }
 
-    /*
     #[test]
-    fn test_struct() {
+    fn test_sequence() {
         #[derive(serde::Deserialize, PartialEq, Debug)]
         struct Test {
-            int: u32,
-            seq: Vec<String>,
+            seq: Vec<u32>,
         }
 
         let j = PersistentDataRecord {
             _TokenOffset: 0,
             _ArgOffset: 0,
             tokens: vec![
-                Tokens::UINT_TOKEN("int".to_string()),
-                Tokens::STRING_TOKEN("seq".to_string()),
-                Tokens::STRING_TOKEN("seq".to_string()),
+                Tokens::UINT_TOKEN("seq".to_string()),
+                Tokens::UINT_TOKEN("seq".to_string()),
+                Tokens::UINT_TOKEN("seq".to_string()),
             ],
             args: vec![1, 2, 3],
-            strings: vec![
-                "int".to_string(),
-                "seq".to_string(),
-                "a".to_string(),
-                "b".to_string(),
-            ],
+            strings: vec!["seq".to_string()],
         };
-        let expected = Test {
-            int: 1,
-            seq: vec!["a".to_owned(), "b".to_owned()],
-        };
+        let expected = Test { seq: vec![1, 2, 3] };
         assert_eq!(expected, from_pdr(&j).unwrap());
     }
-
-    #[test]
-    fn test_enum() {
-        #[derive(Deserialize, PartialEq, Debug)]
-        enum E {
-            Unit,
-            Newtype(u32),
-            Tuple(u32, u32),
-            Struct { a: u32 },
-        }
-
-        let j = r#""Unit""#;
-        let expected = E::Unit;
-        assert_eq!(expected, from_str(j).unwrap());
-
-        let j = r#"{"Newtype":1}"#;
-        let expected = E::Newtype(1);
-        assert_eq!(expected, from_str(j).unwrap());
-
-        let j = r#"{"Tuple":[1,2]}"#;
-        let expected = E::Tuple(1, 2);
-        assert_eq!(expected, from_str(j).unwrap());
-
-        let j = r#"{"Struct":{"a":1}}"#;
-        let expected = E::Struct { a: 1 };
-        assert_eq!(expected, from_str(j).unwrap());
-    }
-
-     */
 }
--- src/format/mod.rs
+++ src/format/mod.rs
@@ -1,3 +1,2 @@
 mod de;
 mod error;
-
